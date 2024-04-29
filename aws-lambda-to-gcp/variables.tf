c020f5d800577957deca2d8f2a3371d838ce3f57
Khalid Saifullah <khalid@outlook.com.au>
2024-04-29 11:14:18 +1000

    write an object in GCS

--- aws-lambda-to-gcp/variables.tf
+++ aws-lambda-to-gcp/variables.tf
@@ -12,3 +12,18 @@ variable "aws_region" {
   type        = string
   description = "The AWS region to deploy resources."
 }
+
+variable "google_project_id" {
+  type        = string
+  description = "The Google Cloud project ID."
+}
+
+variable "gcp_identity_pool" {
+  type        = string
+  description = "The Google Cloud identity pool name."
+}
+
+variable "gcp_identity_provider" {
+  type        = string
+  description = "The Google Cloud identity provider name."
+}

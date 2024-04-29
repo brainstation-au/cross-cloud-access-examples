254aa11ccd36254661c96725a91c6f51868f1a79
Khalid Saifullah <khalid@outlook.com.au>
2024-04-29 11:28:58 +1000

    write an object in GCS (#6)

---
+++ aws-lambda-to-gcp/gcp.tf
@@ -0,0 +1,14 @@
+resource "google_storage_bucket" "this" {
+  name                        = local.app_name
+  location                    = "AUSTRALIA-SOUTHEAST1"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket_iam_member" "this" {
+  bucket = google_storage_bucket.this.name
+  role   = "roles/storage.objectAdmin"
+  member = "principalSet://iam.googleapis.com/${var.gcp_identity_pool}/attribute.aws_role/${aws_iam_role.this.name}"
+}
--- aws-lambda-to-gcp/lambda/main.py
+++ aws-lambda-to-gcp/lambda/main.py
@@ -1,2 +1,59 @@
+import json
+import os
+import urllib
+
+import requests
+import boto3
+
+from botocore.auth import SigV4Auth
+from botocore.awsrequest import AWSRequest
+from google.cloud import storage
+from google.oauth2.credentials import Credentials
+
+
+def get_access_token(google_identity_provider: str) -> dict:
+    audience = f"//iam.googleapis.com/{google_identity_provider}"
+    aws_request = AWSRequest(
+        method="POST",
+        url="https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15",
+        headers={
+            "Host": "sts.amazonaws.com",
+            "x-goog-cloud-target-resource": audience,
+        },
+    )
+
+    SigV4Auth(boto3.Session().get_credentials(), "sts", "us-east-1").add_auth(aws_request)
+    token = {"url": aws_request.url, "method": aws_request.method, "headers": []}
+    for key, value in aws_request.headers.items():
+        token["headers"].append({"key": key, "value": value})
+
+    payload = {
+        "audience": audience,
+        "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
+        "requested_token_type": "urn:ietf:params:oauth:token-type:access_token",
+        "scope": "https://www.googleapis.com/auth/cloud-platform",
+        "subject_token_type": "urn:ietf:params:aws:token-type:aws4_request",
+        "subject_token": urllib.parse.quote(json.dumps(token)),
+    }
+    response = requests.post(url="https://sts.googleapis.com/v1/token", data=json.dumps(payload))
+    return response.json().get("access_token")
+
+def write_object(project_id: str, access_token: str, bucket_name: str, object_name: str, data: str) -> None:
+    storage_client = storage.Client(
+        project=project_id,
+        credentials=Credentials(access_token),
+    )
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(object_name)
+    blob.upload_from_string(data)
+
+def main() -> None:
+    google_identity_provider = os.getenv("GOOGLE_IDENTITY_PROVIDER")
+    access_token = get_access_token(google_identity_provider)
+
+    google_project_id = os.getenv("GOOGLE_PROJECT_ID")
+    bucket_name = os.getenv("GCS_BUCKET_NAME")
+    write_object(google_project_id, access_token, bucket_name, "foo/bar/test.txt", "Hello, World!")
+
 def handler(event, context):
-    print("Hello, World!")
+    main()
